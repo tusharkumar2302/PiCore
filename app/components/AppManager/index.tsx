34ec53f4c05662f009f4c03c49ed9f936a24c81a
tusharkumar2302 <tusharkumar27864@gmail.com>
2025-03-09 19:34:31 +0530

    Created components files

---
+++ app/components/AppManager/index.tsx
@@ -0,0 +1,6 @@
+import React , {memo, ReactElement, useEffect} from 'react';
+import { Appearance, View } from 'react-native';
+
+// App Modules
+import styles from './styles';
+import AppearancePreferences = Appearance.AppearancePreferences;
